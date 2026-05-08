8ee03f1937cc670796df684698c3f0ea8ec4b04c
raziq <raziqsufian2002@gmail.com>
2026-05-08 09:37:24 +0800

    intialize database in main

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,20 @@ from logger import logger
 from middlewares.exceptions import catch_exceptions_middleware
 from routes.ask import router as ask_router
 from routes.uploads import router as uploads_router
+from database import engine, Base
+from models import user
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info("🚀 Starting HealthSideKick Server...")
+    logger.info("🔧 Initializing database...")
+
+    try:
+        Base.metadata.create_all(bind=engine)
+        logger.info("✅ Database initialized successfully!")
+    except Exception as e:
+        logger.error(f"❌ Database initialization failed: {e}")
+
     yield
     logger.info("🛑 Shutting down HealthSideKick Server...")
 
