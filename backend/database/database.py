531e8d2bddfdf9d65904df62b15b5dc630f4cc2a
raziq <raziqsufian2002@gmail.com>
2026-05-08 09:20:44 +0800

    initiate database

---
+++ backend/database/database.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+        
